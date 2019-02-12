1cd5f57783d9e2a0447db34d586d1140cb3161ad
Blaz <mb250653@ncr.com>
2019-02-12 16:10:06 -0500

    damn

--- js/main.js
+++ js/main.js
@@ -5,13 +5,34 @@ var view = {
         var messageArea = document.querySelector('#messageArea');
         messageArea.innerHTML = msg;
     },
-    //If player misses, MISS will be placed on the screen
-    displayMiss: function(location){
-
-    },
     //if player hits, ship will be displayed on the field player hit
     displayHit: function(location){
-        //code motherfucker
-    }
-}
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
 
+    //If player misses, MISS will be placed on the screen
+    displayMiss: function(location){
+        var cell = document.getElementById(location);
+        cell.setAttribute("class","miss");
+    }
+};
+
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength:3,
+    shipSunk:0,
+
+    ships: [{locations: ["06", "16", "26"], hits: ["", "", ""]},
+            {locations: ["24", "34", "44"], hits: ["", "", ""]},
+            {locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+    fire: function (guess){
+        
+        for(var i = 0; i < this.numShips; i++){
+            var ship = this.ships[i];
+        }
+    }
+};
